9132c09173aff2f5ef5898571bf18c4b3e216afe
4orbank <tanakhan@gmail.com>
2025-08-01 12:08:59 +0700

    new

--- lab_6/lab6.cpp
+++ lab_6/lab6.cpp
@@ -4,9 +4,11 @@ int main(){
     int N;
     printf("Enter the number : ");
     scanf("%d", &N);
+    printf("Error: Please enter a valid integer number.\n");
     if ( N % 2 != 0)
         for (int i = 1 ; i <= N; i += 2) {
             printf("%d ", i);
+            return 1;
         }
     else
         for (int i = N; i >= 0 ; i -= 2) {
