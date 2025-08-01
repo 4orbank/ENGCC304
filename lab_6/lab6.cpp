7103f5649f3be979a7be4744dd8081c4514b44c4
4orbank <tanakhan@gmail.com>
2025-08-01 11:32:11 +0700

    Lab 6 upload

---
+++ lab_6/lab6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    printf("Enter the number : ");
+    scanf("%d", &N);
+    if ( N % 2 != 0)
+        for (int i = 1 ; i <= N; i += 2) {
+            printf("%d ", i);
+        }
+    else
+        for (int i = N; i >= 0 ; i -= 2) {
+            printf("%d ", i);
+        }
+    return 0;
+}
